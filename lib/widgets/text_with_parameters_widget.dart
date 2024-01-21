c3ca20f6df81502b7c481fa4699ee5e0a90a76b2
Enes <enesakbal00@gmail.com>
2024-01-21 18:23:58 +0300

    feat: TextWithParametersWidget to display text with dynamic parameters

---
+++ lib/widgets/text_with_parameters_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:l10n_example/l10n/l10n.dart';
+import 'package:l10n_example/widgets/_custom_text_widget.dart';
+
+class TextWithParametersWidget extends StatelessWidget {
+  const TextWithParametersWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return CustomText(
+      title: l10n.textWithParametersTitle,
+      content: Text(
+        l10n.textWithParametersContent(
+          'Enes',
+          22,
+          2.5,
+          DateTime(2001, 07, 29),
+          'Istanbul, Türkiye',
+        ),
+      ),
+    );
+  }
+}
